d6c6c1b5b12e50401812411cf0a0aef95e734e48
KevinDGnanih <kevin.gnanih@gmail.com>
2022-01-03 00:37:34 +0000

    Creating a better welcome message in the game

--- run.py
+++ run.py
@@ -1,11 +1,23 @@
+class GameBoard:
+    """
+    Main board class. Sets board size, the number of ships,
+    the player's name and the board type (player board or computer).
+    Has also methods for adding ships and guesses and printing the board
+    """
+    def __init__(self, size, num_ships, name, type):
+        self.size = size
+        self.num_ships = num_ships
+        self.name = name
+        self.type = type
+
+
 def get_player_name():
     """
     Get name input from player
     """
     while True:
 
-        print("Please enter a name with no more than 10 characters")
-        get_name = input("Please enter your name:\n")
+        get_name = input("Please enter a name with no more than 10 characters:\n")
         name_is = get_name
 
         if validate_name(name_is):
@@ -39,11 +51,18 @@ def new_game():
     Sets the board size and number of ships, resets
     the scores and initialises the boards.
     """
-    print("-" * 35)
-    print("Welcome to BATTLESHIP WAR GAME !!")
-    print("Board size: 6. Number of ships: 5")
-    print("Top left corner is row: 0, col: 0")
-    print("-" * 35)
+    size = 6
+    num_ships = 5
+    print("+", "-" * 35, "+")
+    print("   Welcome to BATTLESHIP WAR GAME !!")
+    print(f"   Board size: {size}. Number of ships: {num_ships}")
+    print("   Top left corner is row: 0, col: 0")
+    print("+", "-" * 35, "+")
+    get_player_name()
+
+    ai_board = GameBoard(size, num_ships, "AI", type="AI")
+
+
 
 
 new_game()
